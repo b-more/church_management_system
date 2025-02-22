91a11f94549fc8e864267133e20f88c95242c19f
b-more <mulengablessmore@gmail.com>
2025-02-22 14:28:19 +0200

    added the manifest

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://cms.crmzambia.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
